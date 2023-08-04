c343f9845b39a94a015aa193dd216cbe0959579e
Burak SAHIN <burak.sahin@pusula.com.tr>
2023-08-04 20:34:36 +0300

    initial state is unkown by me :(

--- src/boot/tanstack.ts
+++ src/boot/tanstack.ts
@@ -1,16 +1,8 @@
 import { boot } from 'quasar/wrappers'
-import { QueryClient, VueQueryPlugin, hydrate, dehydrate } from '@tanstack/vue-query'
+import { QueryClient, VueQueryPlugin } from '@tanstack/vue-query'
 
-export default boot(({ app, ssrContext }) => {
+export default boot(({ app }) => {
   const queryClient = new QueryClient()
-
-  if (process.env.SERVER) {
-    // Indicate how to access and serialize VueQuery state during SSR
-    initialState.vueQueryState = { toJSON: () => dehydrate(queryClient) }
-  } else {
-    // Reuse the existing state in the browser
-    hydrate(queryClient, initialState.vueQueryState)
-  }
 
   app.use(VueQueryPlugin, { queryClient })
 })
