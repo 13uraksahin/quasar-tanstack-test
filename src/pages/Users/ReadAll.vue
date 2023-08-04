d2fa58ddec7dc59a0371dd2016add3e117ae5f3f
Burak SAHIN <burak.sahin@pusula.com.tr>
2023-08-04 20:31:03 +0300

    client-side example

--- src/pages/Users/ReadAll.vue
+++ src/pages/Users/ReadAll.vue
@@ -1,22 +1,12 @@
-<script lang="ts">
-import { useQuery } from '@tanstack/vue-query'
-import { preFetch } from 'quasar/wrappers'
-import axios from 'axios'
-
-export default {
-  preFetch: preFetch(async () => {
-    return await useQuery({
-      queryKey: ['users'],
-      queryFn: async () => await axios.get('http://localhost:3000/users')
-    })
-  })
-}
-</script>
-
 <script setup lang="ts">
 import { onMounted } from 'vue'
+import { useQuery } from '@tanstack/vue-query'
+import axios from 'axios'
 
-const { data } = useQuery({ queryKey: ['users'] })
+const { data } = useQuery({
+  queryKey: ['users'],
+  queryFn: async () => await axios.get('http://localhost:3000/users')
+})
 
 onMounted(() => {
   console.log('client - data: ', data)
